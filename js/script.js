46b5b222d9c95b7027e060f94186c5ae6a136413
Arelthia <ap.gwhere@gmail.com>
2017-07-29 22:00:17 -0500

    changed ajax success to a promise. Fixed Reset form after form submitted

--- js/script.js
+++ js/script.js
@@ -5,33 +5,30 @@
   let podcasts = {};
   let quotes ={};
   //let media = {};
-  
-//get  podcasts
-  jQuery.ajax({
-    url: ajaxUrl + '/podcast/?per_page=6&_embed=true',
-    method: 'GET',
-    success: function(res) {
-     
-      podcasts = res;
-      showAllPodcasts(podcasts);
-     
-    }//end success 
-  });//end ajax
+  jQuery( document ).one( "pagecreate", "#podcasts", function(){
+    //get  podcasts
+      jQuery.ajax({
+        url: ajaxUrl + '/podcast/?per_page=6&_embed=true',
+        method: 'GET',
+        success: function(res) {
+         
+          podcasts = res;
+           showAllPodcasts(podcasts);
+         
+        }//end success 
+      });//end ajax
 
-//get image quotes
-jQuery.ajax({
-    url: ajaxUrl + '/image_quote/?per_page=6&_embed=true',
-    method: 'GET',
-    success: function(res) {
-     
-      quotes = res;
-     //showQuotes(res);
-     
-    }//end success 
-  });//end ajax
+    //get image quotes
+    
+
+ }); //end firstpage pagecreate
+
+jQuery( document ).one( "pageshow", "#podcasts", function(){
+ 
+}); //end firstpage pagecreate
 
   // When one of the podcast blocks clicked
-     jQuery(document).on( "click swipeleft swiperight", "#podcasts .one-half" ,  function(e) {
+  jQuery(document).on( "click swipeleft swiperight", "#podcasts .one-half" ,  function(e) {
               
         let pid = this.dataset.id;
         jQuery("#podcasts ul").html("").html("Loading ... ");
@@ -50,7 +47,7 @@ jQuery.ajax({
         //jQuery(this).toggle();
         //jQuery('.back').toggle();
         showAllPodcasts(podcasts);
-      });  //end on back button clicked
+  });  //end on back button clicked
   
   function showAllPodcasts(res){ 
         let fimg='';
@@ -98,7 +95,11 @@ jQuery.ajax({
           quotecontent = '<img src="' + qimg + '" data-pin-nopin="true" /><h2 class="episode-title wrap">' + value.title.rendered + '</h2>';
         
           //jQuery("#quolist").append('<a href="#" class="post-btn"><li class="" data-id="'+value.id+'"  data-transition="slide">' +quotecontent + '</li></a>');    
-          jQuery("#quolist").append('<li class="ui-li-has-thumb"><a href="#singlequote" class="quotes ui-btn ui-btn-icon-right ui-icon-carat-r" id="'+value.id+'"><img src="' + qimg + '" alt="' + value._embedded['wp:featuredmedia'][0].alt_text + '" data-pin-nopin="true"><h2>' + value.title.rendered + '</h2><p>' + value['ddws-quote'] + '</p></a></li>');
+          //jQuery("#quolist").append('<li class="ui-li-has-thumb"><a href="#singlequote" class="quotes ui-btn ui-btn-icon-right ui-icon-carat-r" id="'+value.id+'"><img src="' + qimg + '" alt="' + value._embedded['wp:featuredmedia'][0].alt_text + '" data-pin-nopin="true"><h2>' + value.title.rendered + '</h2><p>' + value['ddws-quote'] + '</p></a></li>');
+     
+          jQuery("#image-quotes").append('<img src="' + qimg + '" alt="' + value._embedded['wp:featuredmedia'][0].alt_text + '" >');
+     
+
       })
   }
   
@@ -110,29 +111,62 @@ jQuery.ajax({
     e.preventDefault();
     let formurl = 'https://230.pintophosting.com/gravityformsapi/forms/3/submissions';
     let forminput = {
-            "input_4_1": jQuery('#choice_1_4_1:checked').val(),
-            "input_1_3": jQuery('#input_1_1_3').val(),
-            "input_1_6": jQuery('#input_1_1_6').val(),
-            "input_2": jQuery('#input_1_2').val(),
-            "input_3": jQuery('#input_1_3').val(),
-            "input_5": jQuery('#input_3_5').val()
+                "input_4_1": jQuery('#choice_1_4_1:checked').val(),
+                "input_1_3": jQuery('#input_1_1_3').val(),
+                "input_1_6": jQuery('#input_1_1_6').val(),
+                "input_2": jQuery('#input_1_2').val(),
+                "input_3": jQuery('#input_1_3').val(),
+                "input_5": jQuery('#input_3_5').val()
     };
-    //console.log(forminput);
+        //console.log(forminput);
     let datar = {'input_values': forminput};
 
-    //get  podcasts
-    jQuery.ajax({
-      url: formurl,
-      type: 'POST',
-      data: JSON.stringify(datar)
+    let formResponse = sendQuestion(formurl, datar);
+    formResponse.complete(function(data) {
+        jQuery('form')[0].reset();
+        location.href = '#firstpage';
+        
     });
 
-   location.href = '#firstpage';
-   //jQuery('form')[0].reset();
+   
+   
 
    
   });//end on form submit
 
+
+  jQuery( document ).one( "pagecreate", ".quote-page", function(){
+      jQuery( "#header" ).toolbar({ theme: "b" });
+      jQuery( "#footer" ).toolbar({ theme: "b" });
+
+      
+     jQuery.ajax({
+        url: ajaxUrl + '/image_quote/?per_page=6&_embed=true',
+        method: 'GET',
+        success: function(res) {
+         
+        //quotes = res;
+         showQuotes(res);
+         
+        }//end success 
+      });//end ajax
+
+    
+
+
+    });
+
+
+//submit question
+  function sendQuestion(formurl, datar){
+        
+
+        return jQuery.ajax({
+          url: formurl,
+          type: 'POST',
+          data: JSON.stringify(datar)
+        });
+   } //end sendQuestion
 
 })();
 
